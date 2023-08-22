fabb5eb6fd2b90bc66fbcbdf604ef0ec80f474e5
lainemarkus <markus-laine@aalto.fi>
2023-08-22 14:51:14 +0300

    Fixed small error with the order of app.use calls

--- osa4/bloglist/app.js
+++ osa4/bloglist/app.js
@@ -17,14 +17,14 @@ mongoose.set('strictQuery', false)
 app.use(cors())
 app.use(express.json())
 
-app.use(middleware.requestLogger)
-app.use(middleware.unknownEndpoint)
-app.use(middleware.errorHandler)
-app.use(middleware.tokenExtractor)
-
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
+
+app.use(middleware.requestLogger)
+app.use(middleware.errorHandler)
+app.use(middleware.tokenExtractor)
+app.use(middleware.unknownEndpoint)
 
 mongoose.connect(config.MONGODB_URI)
   .then(() => {
